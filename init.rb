77c3e1db85ff86ceed042e985f801cae54d42fe1
Cameron Banta <cbanta@gmail.com>
2010-03-11 16:08:58 -0600

    Initial Commit

---
+++ init.rb
@@ -0,0 +1,12 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_git_links do
+  name 'Redmine Git Links plugin'
+  author 'Cameron Banta'
+  description 'This plugin allows you to change where the git revision link points to (to use cgit for example)'
+  version '0.0.1'
+  settings :default =>{
+      'git_link_url' => '/cgit/project/commit/?id=%revid%',
+      'git_link_url_on' => true,
+    }, :partial => 'settings/git_links_plugin_settings'
+end

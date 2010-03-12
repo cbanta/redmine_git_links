e1347cfe057a85e64d65a627be485055ac34f068
Cameron Banta <cbanta@gmail.com>
2010-03-12 07:19:48 -0600

    Add support for project name and project id

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :redmine_git_links do
   description 'This plugin allows you to change where the git revision link points to (to use cgit for example)'
   version '0.0.1'
   settings :default =>{
-      'git_link_url' => '/cgit/%project%/commit/?id=%rev%',
+      'git_link_url' => '/cgit/%project_name%/commit/?id=%rev%',
       'git_link_url_on' => true,
     }, :partial => 'settings/git_links_plugin_settings'
 end
